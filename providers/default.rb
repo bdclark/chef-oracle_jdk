0748a1c3660f136d64d5c6778577d724cc34009f
Brian Clark <brian@clark.zone>
2015-01-02 18:29:16 -0500

    not using shell_out

--- providers/default.rb
+++ providers/default.rb
@@ -16,9 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'chef/mixin/shell_out'
-include Chef::Mixin::ShellOut
-
 use_inline_resources if defined?(use_inline_resources)
 
 def whyrun_supported?
