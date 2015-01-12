0c867e0213382ef954a8615a93e044a101a749a2
Brian Clark <brian@clark.zone>
2015-01-12 08:31:16 -0500

    integration tests for amazon linux

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'apache2'
 description      'Installs/Configures oracle_jdk'
 long_description 'Installs/Configures oracle_jdk'
 version          '0.1.0'
+
+%w(centos amazon ubuntu).each { |os| supports os }
